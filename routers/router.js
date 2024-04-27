84bcfe96be701ad832b708ea1a832e6f33bfebc6
Nandan Magdum <nandanmagdum14@gmail.com>
2024-04-27 23:16:46 +0530

    updated the api url

--- routers/router.js
+++ routers/router.js
@@ -4,6 +4,10 @@ const express  = require("express");
 
 const router = express.Router();
 
+router.get("/", (req, res) => {
+    return res.status(200).json("Server is live");
+});
+
 router.use("/auth", authRouter);
 
 module.exports = router;
