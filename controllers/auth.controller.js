1bc584f1de5e420f944ee83f3776f871ae0cfffd
Nandan Magdum <nandanmagdum14@gmail.com>
2024-05-18 19:27:28 +0530

    changed auth , create user gives jwt

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -5,11 +5,33 @@ require("dotenv").config;
 const createUserController = async(req, res) => {
     const data = req.body;
     try {
-        const newUser = await userModel.create(data);
-        if(!newUser){
+        const user = await userModel.create(data);
+        if(!user){
             return res.status(400).json("Error creating new user");
         }
-        return res.status(200).json(newUser);
+        // return res.status(200).json(newUser);
+        // create json web token
+        const token = await jwt.sign(
+            {
+            name: user.name,
+            phone: user.phone,
+            uuid: user.uuid,
+            address: {
+                street: user.address.street,
+                village_city : user.address.village_city,
+                taluka: user.address.taluka,
+                district : user.address.district,
+                zip: user.address.zip,
+                state : user.address.state
+            }
+        }
+        , process.env.JWT_KEY, {expiresIn: '30d'});
+
+        if(!token){
+            return res.status(400).json("error creating jwt token");
+        }
+        //return in response
+        return res.status(200).json(token);
     } catch (error) {
         console.log(error.message);
         return res.status(400).json(error.message);
